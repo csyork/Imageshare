52064d4475bc11aa1af950de8f4a1629d2614dab
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-11 21:55:26 +0000

    Added comments route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'photos#index'
   
-   resources :photos
+   resources :photos do
+    resources :comments
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
