e5ef2d2a8d800c4f3b8369cca4410a886ed8f10f
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-11 21:54:19 +0000

    Added associated comments model and migrated DB

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ApplicationRecord
     
     
     has_attached_file :image  
-    
+    has_many :comments
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
