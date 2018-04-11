fabe06d9750477df57e97f7268b61d4fddd7a0e3
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-11 22:14:22 +0000

    Add destroy function for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
         redirect_to photo_path(@photo)
     end
     
+    def destroy
+        @photo = Photo.find(params[:photo_id])
+        @comment = @photo.comments.find(params[:id])
+        @comment.destroy
+        redirect_to photo_path(@photo)
+    end
+    
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ApplicationRecord
     
     
     has_attached_file :image  
-    has_many :comments
+    has_many :comments, dependent: :destroy
+    
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
