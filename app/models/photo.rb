bd54569a8a3d2454828625d8e2efb7634066dac5
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-23 16:50:20 +0000

    Fixed Title bug. Titles now show

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
     
     
-    has_attached_file :image  
+    has_attached_file :image 
     has_many :comments, dependent: :destroy
     
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
