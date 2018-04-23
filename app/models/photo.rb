eff76a864099b81c7662e0ec9492ce70a0e27523
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-23 17:11:26 +0000

    Added asocciated model for reports

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,7 @@ class Photo < ApplicationRecord
     
     has_attached_file :image 
     has_many :comments, dependent: :destroy
+    has_many :reports
     
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
