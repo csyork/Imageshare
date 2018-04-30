88612345366b142ddd30b62ade1d291482651d5e
EC2 Default User <ec2-user@ip-172-31-14-192.ec2.internal>
2018-04-30 03:48:31 +0000

    Fixed validation error for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,7 @@ class Photo < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :reports, dependent: :destroy
     
-    
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :image, presence: true
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
